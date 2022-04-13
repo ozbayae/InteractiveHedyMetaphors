4cc7354a899990308dad150f1d5c1d167b3c2702
A.E. Özbay <s1878549@vuw.leidenuniv.nl>
2022-04-13 22:42:10 +0200

    ToBuild

--- Assets/Scripts/InteractablesManager.cs
+++ Assets/Scripts/InteractablesManager.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using Newtonsoft.Json;
-using UnityEditor;
-using UnityEditor.PackageManager;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class InteractablesManager : MonoBehaviour
 {
